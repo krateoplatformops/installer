3e486ee4f9fb6edd440f618cc32ece4aecb6531c
Luca Sepe <luca.sepe@gmail.com>
2024-05-10 14:24:14 +0200

    feat: enable debug flag by default (#22)

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,4 +5,4 @@ kubectl apply -f testdata/ns.yaml
 kubectl apply -f testdata/vcluster.yaml
 
 
-go run cmd/main.go --debug --poll "2m" -n "krateo-system"
+go run cmd/main.go --poll "2m" -n "krateo-system"
