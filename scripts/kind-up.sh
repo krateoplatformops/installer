a9edc10ca8b1d6f01dba137099bbbb4d104211db
Diego Braga <diego.braga86@gmail.com>
2025-12-10 10:33:59 +0100

    feat: cleanup (#59)

--- scripts/kind-up.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-kind get kubeconfig >/dev/null 2>&1 || kind create cluster --wait 120s --config $SCRIPT_DIR/kind.yaml
-
