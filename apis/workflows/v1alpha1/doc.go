373da72401fda5e9aa2da55240166ba5b2ae758f
Diego Braga <diego.braga86@gmail.com>
2025-12-10 09:30:49 +0100

    feat: cleanup

--- apis/workflows/v1alpha1/doc.go
+++
@@ -1,4 +0,0 @@
-// +kubebuilder:object:generate=true
-// +groupName=krateo.io
-// +versionName=v1alpha1
-package v1alpha1
--- scripts/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-kubectl apply -f crds/
-kubectl apply -f testdata/ns.yaml
-kubectl apply -f testdata/vcluster.min.yaml
-
-
-go run cmd/main.go --poll "2m" -n "krateo-system"
